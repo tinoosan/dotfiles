591540b7f66e8cedf9eeabd290f535a5783fb82c
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-20 15:03:56 +0100

    feat: changed theme to tokyonight

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 
     options = {
     icons_enabled = true,
-    theme = 'palenight',
+    theme = 'tokyonight',
    section_separators = { left = '', right = '' },
   component_separators = { left = '', right = '' },
     disabled_filetypes = {
