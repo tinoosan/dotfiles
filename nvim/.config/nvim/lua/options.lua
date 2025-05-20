04dd32bb1a1b8066f6125a38a95f82f7ad438d1b
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-20 15:04:39 +0100

    feat: changed theme to tokyonight

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("tokyonight")
 
 -- File explorer
 vim.keymap.set("n", "<leader>fe",
