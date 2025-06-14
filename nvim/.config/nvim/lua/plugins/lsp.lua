29c0405b67e06f54423fbb49a8f02019e8e4513e
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-06-14 11:24:52 +0100

    added null-ls plugin

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,4 @@
-local servers = { "gopls", "lua_ls", "pyright", "ts_ls", "clangd" }
+local servers = { "gopls", "lua_ls", "pyright", "ts_ls", "clangd", "dockerls" }
 
 return {
   {
@@ -59,6 +59,21 @@ return {
   },
 
   {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.goimports,
+          null_ls.builtins.completion.spell,
+        },
+      })
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
+
+  {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
@@ -75,7 +90,9 @@ return {
         keymap("n", "gr", vim.lsp.buf.references, opts)
         keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
         keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        keymap("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, opts)
+        keymap("n", "<leader>f", function()
+          vim.lsp.buf.format({ async = true })
+        end, opts)
         keymap("n", "[d", vim.diagnostic.goto_prev, opts)
         keymap("n", "]d", vim.diagnostic.goto_next, opts)
         keymap("n", "<leader>e", vim.diagnostic.open_float, opts)
@@ -90,4 +107,3 @@ return {
     end,
   },
 }
-
